147550811d229d72bcc15566615cfb9eb64fc26c
왕주영 노트북 <happyboyjoo@gmail.com>
2025-07-02 01:17:07 +0900

    requirements 추가

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from chat import chat
+from .chat import chat
 from retriever import retriever
 
 app = FastAPI()
